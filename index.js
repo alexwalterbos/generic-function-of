c703814e973e608ac283dcdadcf7a381af9fc08d
Alex Walterbos <atw.231@gmail.com>
2019-11-06 09:31:24 +0100

    Created smallest repro of false error

---
+++ index.js
@@ -0,0 +1,11 @@
+function genericFunction<R>(args: R): Array<R> {
+	return [args];
+}
+
+function of<R>(args: R): Array<R> {
+	return [args];
+}
+
+function notOf<R>(args: R): Array<R> {
+	return [args];
+}
